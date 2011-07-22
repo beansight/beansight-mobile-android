5d30316a3fd633582b4cd012b40892b77b4a2403
steren <steren.giannini@gmail.com>
2011-07-22 13:32:35 +0000

    New layout for voting
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Mobile-Android/trunk@996 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight-android/src/com/beansight/android/HomeActivity.java
+++ beansight-android/src/com/beansight/android/HomeActivity.java
@@ -1,15 +1,12 @@
 package com.beansight.android;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import com.beansight.android.api.BeansightApi;
 import com.beansight.android.models.InsightListItem;
@@ -17,22 +14,19 @@ import com.beansight.android.models.InsightListItemResponse;
 
 public class HomeActivity extends Activity {
 	
-	private ArrayAdapter<String> arrayAdapter;
 	private String accessToken;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.insight_vote);
         
 		SharedPreferences prefs = getSharedPreferences(BeansightApplication.BEANSIGHT_PREFS, 0);
 		accessToken = prefs.getString("access_token", null);
         
-		List<String> insightList = new ArrayList<String>();
-		ListView insightListView = (ListView)findViewById(R.id.homeListInsights);
-		arrayAdapter = new ArrayAdapter<String>(this, R.layout.insight_item, insightList);
-		insightListView.setAdapter(arrayAdapter);
+		
+		TextView insightText = (TextView)findViewById(R.id.insightText);
 		
 		InsightListItemResponse insightListItemResponse = null;
 		try {
@@ -48,9 +42,8 @@ public class HomeActivity extends Activity {
 		
 		if (insightListItemResponse != null && insightListItemResponse.getMeta().isAuthenticated() && insightListItemResponse.getResponse() != null) {
 			for (InsightListItem item : insightListItemResponse.getResponse()) {
-				this.arrayAdapter.add(item.getContent());
+				insightText.setText(item.getContent());
 			}
-			this.arrayAdapter.notifyDataSetChanged();
 		}
 
     }
