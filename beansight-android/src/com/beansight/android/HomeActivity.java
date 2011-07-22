f62084ce528c696f59c59e1d48315fce759ee5fe
steren <steren.giannini@gmail.com>
2011-07-22 17:13:10 +0000

    next and previous buttons. List goes to infinity
    git-svn-id: svn://92.243.5.254/srv/d_sterenfr/svn/beansight/Mobile-Android/trunk@997 2e3b6b4b-2413-4334-92b7-7e2fc515c0b6

--- beansight-android/src/com/beansight/android/HomeActivity.java
+++ beansight-android/src/com/beansight/android/HomeActivity.java
@@ -1,22 +1,35 @@
 package com.beansight.android;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.beansight.android.api.BeansightApi;
 import com.beansight.android.models.InsightListItem;
 import com.beansight.android.models.InsightListItemResponse;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements View.OnClickListener{
+	
+	/** store the list of downloaded insights */
+	private List<InsightListItem> insightList;
+	/** the number of insight to ask at every list calls */
+	private static final int INSIGHT_NUMBER = 30;
+	/** iterator pointing to the currently displayed insight */
+	private int currentInsightIndex = 0;
+	
+	private TextView insightText;
 	
 	private String accessToken;
-	
-    /** Called when the activity is first created. */
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +38,27 @@ public class HomeActivity extends Activity {
 		SharedPreferences prefs = getSharedPreferences(BeansightApplication.BEANSIGHT_PREFS, 0);
 		accessToken = prefs.getString("access_token", null);
         
+		insightList = new ArrayList<InsightListItem>();
+		// get an iterator
+		fetchNextInsights();
 		
-		TextView insightText = (TextView)findViewById(R.id.insightText);
-		
+		View b;
+		b = (View) findViewById(R.id.buttonAgree);
+        b.setOnClickListener(this);
+		b = (View) findViewById(R.id.buttonDisagree);
+        b.setOnClickListener(this);
+        
+        insightText = (TextView)findViewById(R.id.insightText);
+
+        next();
+    }
+    
+    /** call Beansight API to get the next insight, and append them to the list */
+    private void fetchNextInsights() {
 		InsightListItemResponse insightListItemResponse = null;
+		
 		try {
-		insightListItemResponse = BeansightApi.list(accessToken, null, null, null, null, null, null, null, null);
+		insightListItemResponse = BeansightApi.list(accessToken, insightList.size(), INSIGHT_NUMBER, null, null, null, null, null, null);
 			// if not authenticated, load the WebView Activity
 			if (insightListItemResponse != null && !insightListItemResponse.getMeta().isAuthenticated()) {
 				startActivity( new Intent(this, WebViewActivity.class) );
@@ -40,12 +68,49 @@ public class HomeActivity extends Activity {
 			// FIXME should handle this better ...
 		}
 		
-		if (insightListItemResponse != null && insightListItemResponse.getMeta().isAuthenticated() && insightListItemResponse.getResponse() != null) {
-			for (InsightListItem item : insightListItemResponse.getResponse()) {
-				insightText.setText(item.getContent());
-			}
+        // populate the insight list
+		if (insightListItemResponse != null) {
+			insightList.addAll(insightListItemResponse.getResponse());
 		}
-
     }
+
+    private void agree() {
+    	Toast.makeText(this, "agree", Toast.LENGTH_SHORT).show();
+    	next();
+    }
+    
+    private void disagree() {
+    	Toast.makeText(this, "disagree", Toast.LENGTH_SHORT).show();
+    	next();
+    }
+    
+    private void next() {
+    	if(currentInsightIndex + 1 < insightList.size()) {
+    		currentInsightIndex++;
+    		insightText.setText(insightList.get(currentInsightIndex).getContent());
+    	} else {
+    		fetchNextInsights();
+    		next();
+    	}
+    }
+    
+    private void previous() {
+    	if(currentInsightIndex - 1 > 0) {
+    		currentInsightIndex--;
+    		insightText.setText(insightList.get(currentInsightIndex).getContent());
+    	}
+    }
+    
+	public void onClick(View v) {
+		switch (v.getId()) {
+        case R.id.buttonAgree:
+        	agree();
+        	break;
+        case R.id.buttonDisagree:
+        	disagree();
+        	break;
+		}
+	}
+	
    
 }
